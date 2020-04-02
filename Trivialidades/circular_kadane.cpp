ec5e47c7d350cbd7a254726ce6e2db5d980c6944
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2020-04-02 16:48:48 -0300

    Create circular_kadane.cpp

---
+++ Trivialidades/circular_kadane.cpp
@@ -0,0 +1,24 @@
+int n;
+
+int greedy(int v[]) {
+	int soma = 0, maior = 0;
+	for(int i = 0; i < n; i++) {
+		soma = max(0, soma + v[i]);
+		maior = max(maior, soma);
+	}
+
+	return maior;
+}
+
+int circular_kadane(){
+	cin >> n;
+	int v[MAX], vi[MAX];
+	
+	for(int i = 0; i < n; i++)
+		cin >> v[i];
+	for(int i = 0; i < n; i++)
+		vi[i] = -v[i];
+
+	int negativeSum = greedy(vi);
+	return max(greedy(v), accumulate(v, v+n, 0) + negativeSum);
+}
