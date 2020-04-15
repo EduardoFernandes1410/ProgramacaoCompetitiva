ad9bd534a74c1fb1db2b5ec8b5568aef2f06b765
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2020-04-15 19:05:02 -0300

    Update inversion_count.cpp

--- Trivialidades/inversion_count.cpp
+++ Trivialidades/inversion_count.cpp
@@ -1,10 +1,10 @@
 /******O(n * logn)******/
-int merge_sort(vector<int> &vetor) {
-	if(vetor.size() == 1) return 0;
+int merge_sort(vector<int> &v) {
+	if(v.size() == 1) return 0;
 
 	vector<int> v1, v2;
-	for(int i = 0; i < vetor.size()/2; i++) v1.push_back(vetor[i]);
-	for(int i = vetor.size()/2; i < vetor.size(); i++) v2.push_back(vetor[i]);
+	for(int i = 0; i < v.size()/2; i++) v1.push_back(v[i]);
+	for(int i = v.size()/2; i < v.size(); i++) v2.push_back(v[i]);
 
 	int n1 = merge_sort(v1);
 	int n2 = merge_sort(v2);
@@ -13,12 +13,12 @@ int merge_sort(vector<int> &vetor) {
 
 	int counter = 0;
 	int c1 = 0, c2 = 0;
-	for(int i = 0; i < vetor.size(); i++) {
+	for(int i = 0; i < v.size(); i++) {
 		if(v1[c1] <= v2[c2]) {
-			vetor[i] = v1[c1];
+			v[i] = v1[c1];
 			c1++;
 		} else {
-			vetor[i] = v2[c2];
+			v[i] = v2[c2];
 			c2++;
 
 			counter += (v1.size() - c1 - 1);
@@ -28,3 +28,4 @@ int merge_sort(vector<int> &vetor) {
 	return counter + n1 + n2;
 }
 
+//v = array of values
