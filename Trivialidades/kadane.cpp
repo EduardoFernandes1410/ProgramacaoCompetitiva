3833a6d2a6cf82161eca7a4d7bdadf12ef86f7e4
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2020-04-15 19:06:24 -0300

    Update kadane.cpp

--- Trivialidades/kadane.cpp
+++ Trivialidades/kadane.cpp
@@ -1,10 +1,13 @@
 /******O(n)******/
 int n, c[MAX];
 
-int maior = 0, soma = 0;
+int mx = 0, sum = 0;
 for(int i = 0; i < n; i++) {
-	soma = max(0, soma+c[i]);
-	maior = max(soma, maior);
+	sum = max(0, sum+c[i]);
+	mx = max(sum, mx);
 }
 
-return maior;
+return mx;
+
+//n = number of values
+//c = array of values
