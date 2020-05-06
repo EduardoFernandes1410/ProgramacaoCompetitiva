c1ac0d4b3458f12a5ea2ea5808272e6dda13549e
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2020-05-06 17:34:14 -0300

    Update segtree.cpp

--- Estruturas de Dados/segtree.cpp
+++ Estruturas de Dados/segtree.cpp
@@ -1,5 +1,7 @@
 vector<int> seg(4*MAX), lazy(4*MAX, 0), v(MAX);
 
+/******O(logn)******/
+//update array to 'v' from 'lt' to 'rt'
 int update(int i, int l, int r, int lt, int rt, int v) {
 	if(lazy[i] > 0) {
 		seg[i] = (r - l + 1) * lazy[i];
@@ -21,6 +23,8 @@ int update(int i, int l, int r, int lt, int rt, int v) {
 	return seg[i] = seg[2*i] + seg[2*i + 1];
 }
 
+/******O(logn)******/
+//return sum of values from 'lt' to 'rt'
 int query(int i, int l, int r, int lt, int rt) {
 	if(lazy[i] > 0) {
 		seg[i] = (r - l + 1) * lazy[i];
@@ -35,6 +39,8 @@ int query(int i, int l, int r, int lt, int rt) {
 	return query(2*i, l, meio, lt, rt) + query(2*i + 1, meio + 1, r, lt, rt);
 }
 
+/******O(logn)******/
+//builds segtree
 void build(int i, int l, int r) {
 	if(l == r) {
 		seg[i] = v[l];
@@ -47,3 +53,8 @@ void build(int i, int l, int r) {
 
 	seg[i] = seg[2*i] + seg[2*i + 1];
 }
+
+build(1, 1, n); //build
+update(1, 1, n, a, b, k); //update to 'k' from 'a' to 'b'
+query(1, 1, n, a, b); //get sum from 'a' to 'b'
+//indices from 1 to n
