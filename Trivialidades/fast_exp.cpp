2058df69659fbadc8cb7a6f51e5a23d426aa883e
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2020-05-12 16:56:10 -0300

    Update fast_exp.cpp

--- Trivialidades/fast_exp.cpp
+++ Trivialidades/fast_exp.cpp
@@ -1,8 +1,20 @@
 /******O(logn)******/
-int exp(int base, int expo) {
+//recursive
+ll exp(ll base, ll expo) {
 	if(expo == 0) return 1;
 	if(expo == 1) return base;
 
 	if(expo % 2 == 0) return exp(base * base, expo / 2);
 	else return base * exp(base * base, (expo - 1) / 2);
+}
+
+//iterative (x = base, y = expo)
+ll fexp(ll x, ll y) {
+	ll r = 1;
+	while (y) {
+		if (y & 1) r = (r*x);
+		y >>= 1;
+		x = (x*x);
+	}
+	return r;
 }
