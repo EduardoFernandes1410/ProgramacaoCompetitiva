9d44255e9e8b5a58bde66deadfa39cdb2a3eb91e
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2020-06-24 23:27:36 -0300

    Create sieve.cpp

---
+++ Matematica/sieve.cpp
@@ -0,0 +1,15 @@
+vector<int> primes;
+bool sieve[MAX];
+
+/******O(MAX)******/
+void make_sieve() {
+  memset(sieve, 0, sizeof(sieve));
+	for(int i = 2; i < MAX; i++) {
+		if(!sieve[i]) {
+			primes.push_back(i);
+			for(int j = i; j < MAX; j += i) sieve[j] = true;
+		}
+	}
+}
+
+//'vector<int> primes' will then contain all prime numbers in the range [1 to MAX)
