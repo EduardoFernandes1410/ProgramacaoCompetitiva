5f1f2204ac1dc4cc6cb78101c8c54d4cf5bf235b
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2020-07-03 11:49:01 -0300

    Update persistent_segtree.cpp

--- Estruturas de Dados/persistent_segtree.cpp
+++ Estruturas de Dados/persistent_segtree.cpp
@@ -1,3 +1,4 @@
+//struct of a node of the segtree
 struct Node {
 	Node *left, *right;
 	int sum;
@@ -9,11 +10,11 @@ struct Node {
 	}
 };
 
-vector<Node*> nodes;
-vector<ll> v(MAX);
-map<ll, ll> og2new, new2og;
+vector<Node*> nodes; //stores the root nodes for each update query
+vector<ll> v(MAX); //original array
 
 /******O(logn)******/
+//return k-th number in ordered subsequence
 int query(Node *rootl, Node *rootr, int l, int r, int k) {
 	if(l == r) return l;
 
@@ -24,7 +25,8 @@ int query(Node *rootl, Node *rootr, int l, int r, int k) {
 	else return query(rootl->right, rootr->right, meio + 1, r, k-left);
 }
 
-//update segtree
+/******O(logn)******/
+//sum 1 to target node and returns a new root node
 Node* update(Node *root, int l, int r, int target) {
 	if(l == r) return new Node(root->sum + 1);
 
@@ -35,40 +37,10 @@ Node* update(Node *root, int l, int r, int target) {
 
 
 /******O(n*logn)******/
-//builds segtree
+//builds segtree with all sums set to 0
 Node* build(int l, int r) {
 	if(l == r) return new Node(0);
 
 	int meio = (l+r)/2;
 	return new Node(build(l, meio), build(meio + 1, r));
-}
-
-int main() { _
-	int n, m; cin >> n >> m;
-	v.resize(n);
-
-	FOR(i,0,n) cin >> v[i];
-
-	vector<ll> v_sorted = v;
-	sort(v_sorted.begin(), v_sorted.end());
-
-	int diff = 1;
-	for(auto i : v_sorted) {
-		if(og2new.find(i) == og2new.end()) {
-			og2new[i] = diff;
-			new2og[diff] = i;
-			diff++;
-		}
-	}
-
-	nodes.pb(build(1, n));
-	FOR(i,0,n) nodes.pb(update(nodes[i], 1, n, og2new[v[i]]));
-
-	int c1, c2, c3;
-	while(m--) {
-		cin >> c1 >> c2 >> c3;
-		cout << new2og[query(nodes[c1-1], nodes[c2], 1, n, c3)] << endl;
-	}
-
-	return 0;
 }
