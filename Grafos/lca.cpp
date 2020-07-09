d7e15b607e7ea662c479a65d955f0e9d50255fde
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2020-07-09 20:37:16 -0300

    Update lca.cpp

--- Grafos/lca.cpp
+++ Grafos/lca.cpp
@@ -1,3 +1,5 @@
+//REQUIREMENTS: add SegmentTree struct to code (See /Estruturas de Dados/segtree.cpp)
+
 namespace LCA {
 	vector<pair<int, int>> euler;
 	vector<int> height, first;
@@ -5,6 +7,8 @@ namespace LCA {
 
 	SegmentTree<pair<int, int>> seg;
 
+	/******O(n)******/
+	//performes euler tour on tree 'v'
 	void euler_tour(int root, int level, vector<vector<int>> &v) {
 		euler.push_back({level, root});
 		height[root] = level;
@@ -20,6 +24,8 @@ namespace LCA {
 		}
 	}
 
+	/******O(n * logn)******/
+	//builds and initializes everything needed for tree 'v'
 	void build(vector<vector<int>> &v) {
 		int n = v.size();
 		height.resize(n);
@@ -29,14 +35,23 @@ namespace LCA {
 		euler_tour(0, 0, v);
 		seg = SegmentTree<pair<int, int>>(euler, {INF, INF});
 	}
-
+	
+	/******O(logn)******/
+	//returns Lowest Common Ancestor of nodes 'a' and 'b' in tree 'v'
 	int lca(int a, int b) {
 		if(first[a] > first[b]) swap(a, b);
 		return (seg.query(first[a], first[b])).second;
 	}
 
+	/******O(logn)******/
+	//returns distance between nodes 'a' and 'b' in tree 'v'
 	int dist(int a, int b) {
 		int ancestor = lca(a, b);
 		return height[a] + height[b] - (2 * height[ancestor]);
 	}
 };
+
+vector<vector<int>> &v; //adjacency list of tree
+LCA::build(v); //builds LCA stuff
+LCA::lca(a, b);
+LCA::dist(a, b);
