e18729c317d36b34ed7516804ae8585136fa6fd4
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2020-07-10 21:18:06 -0300

    Update segtree.cpp

--- Estruturas de Dados/segtree.cpp
+++ Estruturas de Dados/segtree.cpp
@@ -1,60 +1,90 @@
-vector<int> seg(4*MAX), lazy(4*MAX, 0), v(MAX);
+//Segment Tree with Lazy Propagation
+//Lines with '*todo*' next to them are the only ones that need to change according to your needs
 
-/******O(logn)******/
-//update array to 'value' from 'lt' to 'rt'
-void update(int i, int l, int r, int lt, int rt, int value) {
-	if(lazy[i] > 0) {
-		seg[i] = (r - l + 1) * lazy[i];
-		if(l != r) lazy[2*i] = lazy[2*i + 1] = lazy[i];
-		lazy[i] = 0;
+template<typename T> struct SegmentTree {
+	T neutral;
+	int size;
+	vector<T> seg, lazy;
+
+	SegmentTree() {}
+
+	SegmentTree(vector<T> &v, T neutral_element) {
+		size = v.size();
+		neutral = neutral_element;
+		seg.resize(4 * size);
+		lazy.resize(4 * size, neutral);
+
+		build(v, 1, 0, size-1);
 	}
 
-	if(l > rt or r < lt) return;
-	if(l >= lt and r <= rt) {
-		seg[i] = (r - l + 1) * value;
-		if(l != r) lazy[2*i] = lazy[2*i + 1] = value;
-		return;
+	//merge nodes - ***change function to your needs***
+	T merge(T a, T b) { return a + b; //*todo* }
+
+	//propagate lazy - ***change to your needs***
+	void prop(int i, int l, int r, bool flag = false) {
+		if(lazy[i] != neutral or flag) {
+			seg[i] = (r - l + 1) * lazy[i]; //*todo*
+			if(l != r) {
+				lazy[2*i] = lazy[i]; //*todo*
+				lazy[2*i + 1] = lazy[i]; //*todo*
+			}
+			lazy[i] = neutral;
+		}
 	}
 
-	int meio = (l+r)/2;
-	update(2*i, l, meio, lt, rt, value);
-	update(2*i + 1, meio + 1, r, lt, rt, value);
+	/******O(logn)******/
+	//update segtree
+	void update(int lt, int rt, int value) { update(lt, rt, 1, 0, size-1, value); }
 
-	seg[i] = seg[2*i] + seg[2*i + 1];
-}
+	void update(int lt, int rt, int i, int l, int r, int value) {
+		prop(i, l, r);
 
-/******O(logn)******/
-//return sum of values from 'lt' to 'rt'
-int query(int i, int l, int r, int lt, int rt) {
-	if(lazy[i] > 0) {
-		seg[i] = (r - l + 1) * lazy[i];
-		if(l != r) lazy[2*i] = lazy[2*i + 1] = lazy[i];
-		lazy[i] = 0;
+		if(l > rt or r < lt) return;
+		if(l >= lt and r <= rt) {
+			lazy[i] = value;
+			prop(i, l, r, true);
+			return;
+		}
+
+		int meio = (l+r)/2;
+		update(lt, rt, 2*i, l, meio, value);
+		update(lt, rt, 2*i + 1, meio + 1, r, value);
+
+		seg[i] = merge(seg[2*i], seg[2*i + 1]);
 	}
 
-	if(l > rt or r < lt) return 0;
-	if(l >= lt and r <= rt) return seg[i];
+	/******O(logn)******/
+	//perform query
+	T query(int lt, int rt) { return query(lt, rt, 1, 0, size-1); }
 
-	int meio = (l+r)/2;
-	return query(2*i, l, meio, lt, rt) + query(2*i + 1, meio + 1, r, lt, rt);
-}
+	T query(int lt, int rt, int i, int l, int r) {
+		prop(i, l, r);
 
-/******O(logn)******/
-//builds segtree
-void build(int i, int l, int r) {
-	if(l == r) {
-		seg[i] = v[l];
-		return;
+		if(l > rt or r < lt) return neutral;
+		if(l >= lt and r <= rt) return seg[i];
+
+		int meio = (l+r)/2;
+		return merge(query(lt, rt, 2*i, l, meio), query(lt, rt, 2*i + 1, meio + 1, r));
 	}
 
-	int meio = (l+r)/2;
-	build(2*i, l, meio);
-	build(2*i + 1, meio + 1, r);
+	/******O(logn)******/
+	//builds segtree
+	void build(vector<T> &v, int i, int l, int r) {
+		if(l == r) {
+			seg[i] = v[l];
+			return;
+		}
 
-	seg[i] = seg[2*i] + seg[2*i + 1];
-}
+		int meio = (l+r)/2;
+		build(v, 2*i, l, meio);
+		build(v, 2*i + 1, meio + 1, r);
 
-build(1, 1, n); //build
-update(1, 1, n, a, b, k); //update to 'k' from 'a' to 'b'
-query(1, 1, n, a, b); //get sum from 'a' to 'b'
-//indices from 1 to n
+		seg[i] = merge(seg[2*i], seg[2*i + 1]);
+	}
+};
+	
+vector<YOUR_TYPE> v(n); //base array for segtree
+SegmentTree<YOUR_TYPE> seg(v, NEUTRAL_ELEMENT); //initializes object
+seg.update(l, r, value); //updates segtree in [l,r]
+seg.query(l, r); //performs query in [l,r]
+//indices from 0 to n-1
