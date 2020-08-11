b70f7c00fc8a7d1cdd923b95a8401159d88c636d
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2020-08-11 19:05:37 -0300

    Update burnside.cpp

--- Matematica/burnside.cpp
+++ Matematica/burnside.cpp
@@ -8,13 +8,15 @@ ll fexp(ll x, ll y, ll MOD) {
 	return r;
 }
 
-//Returns modular inverse of 'a' for prime number MOD
+// Returns modular inverse of 'a' for prime number MOD
 ll mod_inv(ll a, ll MOD) {
 	return fexp(a, MOD-2, MOD);
 }
 
 /******O(n)******/
-//Calculates number of different rotating objects, mod a prime number
+// Calculates number of different rotating objects, mod a prime number
+// k = number of colors
+// n = size of object
 ll burnside(ll k, ll n, ll MOD) {
   ll res = 0;
   for(int i = 1; i <= n; i++) res = (res + fexp(k, __gcd((ll) i, n), MOD)) % MOD;
