1017b56a61ca0eb0d65df555a28b268d70cfb3be
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2020-09-21 12:52:14 -0300

    Update knapsack.cpp

--- PD/knapsack.cpp
+++ PD/knapsack.cpp
@@ -1,18 +1,32 @@
-/******O(MAX * MAX)******/
-int p, n;
-ll w[MAX], v[MAX], pd[MAX][MAX];
+int n, c;
+vector<ll> w(n), v(n);
+vector<vector<ll>> dp(n+5, vector<ll>(c+5, 0));
 
-ll solve(int s, int c) {
-	if(c == n || s <= 0) return 0;
-	if(pd[s][c] != -1) return pd[s][c];
-
-	if(s >= w[c])
-		return pd[s][c] = max(v[c] + solve(s - w[c], c + 1), solve(s, c + 1));
-	else
-		return pd[s][c] = solve(s, c + 1);
+/******O(n * c)******/
+// Calculate best aswer and stores it in dp[0][c]
+for(ll i = n-1; i >= 0; i--) {
+	for(ll j = 0; j <= w; j++) {
+		if(j < w[i]) dp[i][j] = dp[i+1][j];
+		else dp[i][j] = max(v[i] + dp[i+1][j-w[i]], dp[i+1][j]);
+	}
 }
 
-//p = max capacity;
+
+/******O(n)******/
+// Recover answer 
+int cur_c = c;
+ll best = dp[0][c];
+vector<int> ans;
+
+for(int i = 0; i < n; i++) {
+	if(dp[i+1][cur_c] < best) {
+		ans.pb(i);
+		cur_c -= w[i];
+		best -= v[i];
+	}
+}
+
+//c = max capacity;
 //n = number of items;
 //w = item weights;
 //v = item values;
