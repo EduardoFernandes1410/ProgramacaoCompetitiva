f8134feb7c6538148adcf704affa9fe84fcfee72
Eduardo Augusto Militão Fernandes <eduardo.militao1410@gmail.com>
2021-02-12 16:45:48 -0300

    Update union_find.cpp

--- Estruturas de Dados/union_find.cpp
+++ Estruturas de Dados/union_find.cpp
@@ -1,33 +1,23 @@
 /******O(1)******/
-int parent[MAX], weight[MAX], amount[MAX];
+int parent[MAX], amount[MAX];
 
 int find(int a) {
-	if(parent[a] == a) return a;
-	else return parent[a] = find(parent[a]);
+	return parent[a] = ((parent[a] == a) ? a : find(parent[a]));
 }
 
 void join(int a, int b) {
 	int x = find(a), y = find(b);
 	if(x == y) return;
 
-	if(weight[x] > weight[y]) {
-		parent[y] = x;
-		amount[x] += amount[y];
-	} else if(weight[x] < weight[y]) {
-		parent[x] = y;
-		amount[y] += amount[x];
-	}	else {
-		parent[x] = y;
-		weight[y]++;
-		amount[y] += amount[x];
-	}
+	if(amount[x] < amount[y]) swap(x, y);
+	parent[y] = x;
+	amount[x] += amount[y];
 }
 
 int main() { 
 	int n;
 	for(int i = 1; i <= n; i++) {
 		parent[i] = i;
-		weight[i] = 1;
 		amount[i] = 1;
 	}
 	return 0;
