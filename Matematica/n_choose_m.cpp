935e92c51170f7be1fb59b7666b7728803d3079c
Eduardo Augusto Militão Fernandes <eduardo.militao1410@gmail.com>
2021-10-21 14:33:48 -0300

    Update n_choose_m.cpp

--- Matematica/n_choose_m.cpp
+++ Matematica/n_choose_m.cpp
@@ -3,5 +3,5 @@
 /******O(n*m)******/
 int n, m;
 vector<vector<ll>> dp(n+1, vector<ll>(m+1));
-FOR(i,0,n+1) dp[i][0] = 1;
-FOR(i,1,n+1) FOR(j,1,m+1) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+for(int i = 0; i < n+1; i++) dp[i][0] = 1;
+for(int i = 1; i < n+1; i++) for(int j = 1; j < m+1; j++) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
